792514ab4f67d1235514ab6bbc0fbff6b9a8711e
mKumar <mkumar.rails@gmail.com>
2012-03-29 02:25:04 +0530

    fb-request send

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-MooreDemo::Application.config.secret_token = 'cc614069bb041e9d9e3003e11d34a561154343423aa3635b7bddf96497d57989a2cc037a4a55b3268b3fe89710fb8c2a391a9bfb7014fd1a31d273fd479bd5d3'
+MooreDemo::Application.config.secret_token = 'ef1f8a593c02ef949b4388e125522627' 
